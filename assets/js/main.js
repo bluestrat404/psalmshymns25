e044f42d5e00d61c8008d5dd4b3a18e14bc803b8
bluestrat404 <bluestrat404@users.noreply.github.com>
2025-01-07 17:04:48 -0500

    Update main.js
    1 DAY, not DAYS, left

--- assets/js/main.js
+++ assets/js/main.js
@@ -22,7 +22,7 @@
        ========================================================================== */
      jQuery('#clock').countdown('2025/01/08 23:55',function(event){
       var $this=jQuery(this).html(event.strftime(''
-      +'<div class="time-entry days"><span>%-D</span> Days</div> '
+      +'<div class="time-entry days"><span>%-D</span> Day</div> '
       +'<div class="time-entry hours"><span>%H</span> Hours</div> '
       +'<div class="time-entry minutes"><span>%M</span> Minutes</div> '
       +'<div class="time-entry seconds"><span>%S</span> Seconds</div> '));
